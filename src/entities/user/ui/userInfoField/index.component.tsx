9e0432abf6b2af263efe954bfe4449ab6ed8bb05
haryuuno21 <d.shilenok@mail.ru>
2025-04-25 19:54:30 +0300

    added visibility button for password

--- src/entities/user/ui/userInfoField/index.component.tsx
+++ src/entities/user/ui/userInfoField/index.component.tsx
@@ -1,7 +1,8 @@
 import { InputField } from "@/shared/uikit/inputField";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Props } from "./index.types";
 import { LABELS } from "./index.constants";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 export const UserInfoField: FunctionComponent<Props> = ({
   type,
@@ -11,23 +12,37 @@ export const UserInfoField: FunctionComponent<Props> = ({
   label,
   ...props
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
   return (
     <div>
       <label htmlFor={type} className="block text-sm font-medium text-gray-700">
         {label || LABELS[type]}
       </label>
-      <InputField
-        type={type === "password" ? "password" : "text"}
-        id={name || type}
-        name={name || type}
-        {...(required ? { required } : {})}
-        {...(type === "password" ? { minLength: 8 } : {})}
-        maxLength={64}
-        className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md ${
-          error && `border-red-500 focus:ring-red-500`
-        }`}
-        {...props}
-      />
+      <div className="relative flex flex-row">
+        <InputField
+          type={isPassword ? (showPassword ? "text" : "password") : "text"}
+          id={name || type}
+          name={name || type}
+          {...(required ? { required } : {})}
+          {...(isPassword ? { minLength: 8 } : {})}
+          maxLength={64}
+          className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md ${
+            error && `border-red-500 focus:ring-red-500`
+          }`}
+          {...props}
+        />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-800"
+          >
+            {showPassword ? <Visibility /> : <VisibilityOff />}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
